f840795f1084b21727f08e312488c98a65992f96
hollyyuqizheng <hollyyuqizheng@google.com>
2020-06-26 15:28:10 +0000

    Actually.. workingHours.js isn't that useful of a file, so I moved its code into request.js

--- portfolio/src/main/webapp/workingHours.js
+++
@@ -1,8 +0,0 @@
-/** 
- * Retrieves the working hours from the UI.
- * The getTimeObject funtion is defined in calendar.js.
- */
-function getWorkingHours() {
-  const $workHourStart = getTimeObject($("#working-hour-start").val()); 
-  const $workHourEnd = getTimeObject($("#working-hour-end").val()); 
-}
