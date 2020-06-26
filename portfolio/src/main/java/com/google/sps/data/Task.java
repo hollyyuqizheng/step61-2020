8a00afcbdaeff473323484eda12868bfc8081f5d
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-06-25 20:17:21 -0400

    change task constructors and equals

--- portfolio/src/main/java/com/google/sps/data/Task.java
+++ portfolio/src/main/java/com/google/sps/data/Task.java
@@ -1,6 +1,7 @@
 package com.google.sps.data;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 
 /**
@@ -13,6 +14,7 @@ public final class Task {
   private final Optional<String> description;
   private final Duration duration;
   private final TaskPriority priority;
+  private Instant scheduledTime;
 
   // TODO(raulcruise): Add an ID field if necessary.
 
@@ -20,15 +22,12 @@ public final class Task {
    * The constructor will make sure that all necessary parameters are passed in, and populate each
    * class variable appropriately. None of the fields can be null with exception to the description
    * as it is optional. If the description is passed in as null then it will be stored as an empty
-   * Optional object.
+   * Optional object. Tasks by default do not have a scheduled time, they are only assigned this
+   * field as a result of getting scheduled by the scheduling algorithm.
    */
-  public Task(String name, String description, Duration duration, TaskPriority priority) {
+  public Task(String name, String description, long durationMinutes, TaskPriority priority) {
     if (name == null) {
       throw new IllegalArgumentException("Name cannot be null");
-    }
-
-    if (duration == null) {
-      throw new IllegalArgumentException("Duration cannot be null");
     }
 
     if (priority == null) {
@@ -37,8 +36,33 @@ public final class Task {
 
     this.name = name;
     this.description = Optional.ofNullable(description);
-    this.duration = duration;
+    this.duration = Duration.ofMinutes(durationMinutes);
     this.priority = priority;
+  }
+
+  /**
+   * The constructor allows for Tasks to have their scheduled time be set. New tasks being made
+   * for scheduling purposes use this constructor instead.
+   */
+  public Task(String name, String description, long durationMinutes, TaskPriority priority,
+      String scheduledTime) {
+    if (name == null) {
+      throw new IllegalArgumentException("Name cannot be null");
+    }
+
+    if (priority == null) {
+      throw new IllegalArgumentException("Priority cannot be null");
+    }
+
+    if (scheduledTime == null) {
+      throw new IllegalArgumentException("Scheduled time cannot be null");
+    }
+
+    this.name = name;
+    this.description = Optional.ofNullable(description);
+    this.duration = Duration.ofMinutes(durationMinutes);
+    this.priority = priority;
+    this.scheduledTime = Instant.parse(scheduledTime);
   }
 
   public String getName() {
@@ -53,7 +77,29 @@ public final class Task {
     return duration;
   }
 
-  public Integer getPriority() {
+  public TaskPriority getPriority() {
+    return priority;
+  }
+
+  public Integer getPriorityInt() {
     return priority.getPriority();
+  }
+
+  public long getDurationSeconds() {
+    return duration.getSeconds();
+  }
+
+  // These methods provide a way to tell if two objects are both Task objects
+  // and have all the same fields
+
+  @Override
+  public boolean equals(Object other) {
+    return other instanceof Task && equals(this, (Task) other);
+  }
+
+  private static boolean equals(Task a, Task b) {
+    return a.name.equals(b.name) && a.description.equals(b.description)
+        && a.getDurationSeconds()==b.getDurationSeconds() && a.priority.equals(b.priority)
+        && a.scheduledTime.getEpochSecond()==b.scheduledTime.getEpochSecond();
   }
 }
