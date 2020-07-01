11fd38949fb14dc6d0f95e590424a8230a27f678
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-06-30 21:47:01 -0400

    implement change requests

--- portfolio/src/main/java/com/google/sps/data/ScheduleRequest.java
+++ portfolio/src/main/java/com/google/sps/data/ScheduleRequest.java
@@ -2,9 +2,9 @@ package com.google.sps.data;
 
 import java.time.Instant;
 import java.util.Collection;
+
 /**
- * Models request that are sent to the algorithm. It can be used for both
- * creation and import flow.
+ * Models request that are sent to the algorithm. It can be used for both creation and import flow.
  */
 public class ScheduleRequest {
   private Collection<CalendarEvent> events;
@@ -17,17 +17,19 @@ public class ScheduleRequest {
    *
    * @param events: events happening the day of the scheduling,
    * @param tasks: the tasks a user would like to schedule on that day
-   * @param workHoursStartTimeString: a string representation of the work hours start
-   *     time. The string is in format YYYY-MM-DDTHH:MM:SSZ
-   * @param workHoursEndTimeString: a string representation of the work hours end time.
-   *     The string's format is the same as startTimeString's.
-   * All of these fields are required for a request however, events and tasks can be.
-   * empty in the case where there are no events or no tasks. For example an empty
-   * schedule for that day or the user hits start scheduling before they add any tasks
-   * (maybe by accident).
+   * @param workHoursStartTimeString: a string representation of the work hours start time. The
+   *     string is in format YYYY-MM-DDTHH:MM:SSZ
+   * @param workHoursEndTimeString: a string representation of the work hours end time. The string's
+   *     format is the same as startTimeString's. All of these fields are required for a request
+   *     however, events and tasks can be. empty in the case where there are no events or no tasks.
+   *     For example an empty schedule for that day or the user hits start scheduling before they
+   *     add any tasks (maybe by accident).
    */
-  public ScheduleRequest(Collection<CalendarEvent> events, Collection<Task> tasks,
-      String workHoursStartTimeString, String workHoursEndTimeString) {
+  public ScheduleRequest(
+      Collection<CalendarEvent> events,
+      Collection<Task> tasks,
+      String workHoursStartTimeString,
+      String workHoursEndTimeString) {
     if (events == null) {
       throw new IllegalArgumentException("Events cannot be null");
     }
@@ -55,21 +57,12 @@ public class ScheduleRequest {
   public Collection<Task> getTasks() {
     return tasks;
   }
+
   public Instant getWorkHoursStartTimeInstant() {
     return workHoursStartTime;
   }
+
   public Instant getWorkHoursEndTimeInstant() {
     return workHoursEndTime;
-  }
-
-  // These methods return the instant as a number of seconds from 00:00:00 UTC
-  // on 1 January 1970.
-
-  public long getWorkHoursStartTimeLong() {
-    return workHoursStartTime.getEpochSecond();
-  }
-
-  public long getWorkHoursEndTimeLong() {
-    return workHoursEndTime.getEpochSecond();
   }
 }
