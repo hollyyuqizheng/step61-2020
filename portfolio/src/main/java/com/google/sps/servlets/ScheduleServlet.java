eb4abf31e5c4f867c587b8910a902055b9f77784
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-06-30 21:48:58 -0400

    implement change requests

--- portfolio/src/main/java/com/google/sps/servlets/ScheduleServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ScheduleServlet.java
@@ -20,8 +20,8 @@ import com.google.sps.data.CalendarEvent;
 import com.google.sps.data.ScheduleRequest;
 import com.google.sps.data.Task;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import javax.servlet.annotation.WebServlet;
@@ -30,8 +30,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 @WebServlet("/schedule")
 public class ScheduleServlet extends HttpServlet {
@@ -45,45 +43,47 @@ public class ScheduleServlet extends HttpServlet {
     String workHoursStartTimeString = (String) jsonFromRequest.get("startTime");
     String workHoursEndTimeString = (String) jsonFromRequest.get("endTime");
 
-    Collection<CalendarEvent> events = collectEventsFromJson(eventsArray);
-    Collection<Task> tasks = collectTasksFromJson(tasksArray);
+    Collection<CalendarEvent> events = collectEventsFromJsonArray(eventsArray);
+    Collection<Task> tasks = collectTasksFromJsonArray(tasksArray);
 
     ScheduleRequest scheduleRequest =
         new ScheduleRequest(events, tasks, workHoursStartTimeString, workHoursEndTimeString);
-    FindSchedule findSchedule = new FindSchedule();
-    Collection<Task> scheduledTasks =
-        findSchedule.greedy(scheduleRequest.getEvents(), scheduleRequest.getTasks(),
-            scheduleRequest.getWorkHoursStartTimeLong(), scheduleRequest.getWorkHoursEndTimeLong());
+    Collection<ScheduledTask> scheduledTasks =
+        FindSchedule.shortestTaskFirst(
+            scheduleRequest.getEvents(),
+            scheduleRequest.getTasks(),
+            scheduleRequest.getWorkHoursStartTimeInstant(),
+            scheduleRequest.getWorkHoursEndTimeInstant());
 
     Gson gson = new Gson();
-    String resultJSON = gson.toJson(scheduledTasks);
-    System.out.println(resultJSON);
+    String resultJson = gson.toJson(scheduledTasks);
 
     response.setContentType("application/json");
-    response.getWriter().println(resultJSON);
+    response.getWriter().println(resultJson);
   }
 
-  private static Collection<CalendarEvent> collectEventsFromJson(JSONArray eventsArray) {
+  private static Collection<CalendarEvent> collectEventsFromJsonArray(JSONArray eventsArray) {
     Collection<CalendarEvent> events = new ArrayList<CalendarEvent>();
     for (int i = 0; i < eventsArray.length(); i++) {
-      JSONObject eventAsJSONObject = eventsArray.getJSONObject(i);
-      String name = (String) eventAsJSONObject.get("name");
-      String startTime = (String) eventAsJSONObject.get("startTime");
-      String endTime = (String) eventAsJSONObject.get("endTime");
+      JSONObject eventJsonObject = eventsArray.getJSONObject(i);
+      String name = eventJsonObject.getString("name");
+      String startTime = eventJsonObject.getString("startTime");
+      String endTime = eventJsonObject.getString("endTime");
       CalendarEvent newEvent = new CalendarEvent(name, startTime, endTime);
       events.add(newEvent);
     }
     return events;
   }
 
-  private static Collection<Task> collectTasksFromJson(JSONArray tasksArray) {
+  private static Collection<Task> collectTasksFromJsonArray(JSONArray tasksArray) {
     Collection<Task> tasks = new ArrayList<Task>();
     for (int i = 0; i < tasksArray.length(); i++) {
-      JSONObject taskAsJSONArray = tasksArray.getJSONObject(i);
-      String name = (String) taskAsJSONArray.get("name");
-      String description = (String) taskAsJSONArray.get("description");
-      long duration = Long.valueOf((int) taskAsJSONArray.get("durationMinutes"));
-      int priorityInt = (int) taskAsJSONArray.get("priority");
+      JSONObject taskJsonObject = tasksArray.getJSONObject(i);
+      String name = taskJsonObject.getString("name");
+      String description = taskJsonObject.getString("description");
+      long durationMinutes = taskJsonObject.getLong("duration");
+      Duration duration = Duration.ofMinutes(durationMinutes);
+      int priorityInt = taskJsonObject.getInt("taskPriority");
       TaskPriority priority = new TaskPriority(priorityInt);
       Task newTask = new Task(name, description, duration, priority);
       tasks.add(newTask);
