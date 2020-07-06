c749c6ba80afe93f767a296707fc5cf31820462f
Raul Cruz <raulcollege2@yahoo.com>
2020-07-06 17:48:55 +0000

    Remove unnecessary functions

--- portfolio/src/main/java/com/google/sps/data/Task.java
+++ portfolio/src/main/java/com/google/sps/data/Task.java
@@ -1,7 +1,6 @@
 package com.google.sps.data;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.util.Optional;
 
 /**
@@ -14,66 +13,36 @@ public final class Task {
   private final Optional<String> description;
   private final Duration duration;
   private final TaskPriority priority;
-  private Instant scheduledTime;
 
   // TODO(raulcruise): Add an ID field if necessary.
 
   /**
    * The constructor will make sure that all necessary parameters are passed in, and populate each
-   * class variable appropriately. None of the fields can be null with exception to the description
-   * as it is optional. If the description is passed in as null then it will be stored as an empty
-   * Optional object. Tasks by default do not have a scheduled time, they are only assigned this
-   * field as a result of getting scheduled by the scheduling algorithm.
-   */
-  public Task(String name, String description, Duration duration, TaskPriority priority) {
-    if (name == null) {
-      throw new IllegalArgumentException("Name cannot be null");
-    }
-
-    if (priority == null) {
-      throw new IllegalArgumentException("Priority cannot be null");
-    }
-
-    this.name = name;
-    this.description = Optional.ofNullable(description);
-    this.duration = Duration.ofMinutes(durationMinutes);
-    this.priority = priority;
-  }
-
-  /**
-   * The constructor allows for Tasks to have their scheduled time be set. New tasks being made for
-   * scheduling purposes use this constructor instead.
+   * class variable appropriately.
    *
    * @param name: Name for the Task,
-   * @param description: Optional description for the Task,
+   * @param description: Optional description for the Task, this can be passed in as null but will
+   *     be set the class variable to an empty
    * @param duration: the amount of time the user estimates this Task will require to be completed,
    * @param priority: the priority the user wants the algorithm to consider when creating a
    *     schedule. The priority can range from 1 through 5 with 5 being the highest priority.
    *     Priority is handled by the custom class TaskPriority which checks input values.
    */
-  public Task(
-      String name,
-      String description,
-      Duration duration,
-      TaskPriority priority,
-      String scheduledTime) {
+  public Task(String name, String description, Duration duration, TaskPriority priority) {
     if (name == null) {
       throw new IllegalArgumentException("Name cannot be null");
     }
-
+    if (duration == null) {
+      throw new IllegalArgumentException("Duration cannot be null");
+    }
     if (priority == null) {
       throw new IllegalArgumentException("Priority cannot be null");
     }
 
-    if (scheduledTime == null) {
-      throw new IllegalArgumentException("Scheduled time cannot be null");
-    }
-
     this.name = name;
     this.description = Optional.ofNullable(description);
-    this.duration = Duration.ofMinutes(durationMinutes);
+    this.duration = duration;
     this.priority = priority;
-    this.scheduledTime = Instant.parse(scheduledTime);
   }
 
   public String getName() {
@@ -92,13 +61,8 @@ public final class Task {
     return priority;
   }
 
-  public Integer getPriorityInt() {
-    return priority.getPriority();
-  }
-
-  public Duration getDuration() {
-    return duration.getSeconds();
-  }
+  // These methods provide a way to tell if two objects are both Task objects
+  // and have all the same fields
 
   @Override
   public boolean equals(Object other) {
@@ -108,8 +72,7 @@ public final class Task {
   private static boolean equals(Task a, Task b) {
     return a.name.equals(b.name)
         && a.description.equals(b.description)
-        && a.getDurationSeconds() == b.getDurationSeconds()
-        && a.priority.equals(b.priority)
-        && a.scheduledTime.getEpochSecond() == b.scheduledTime.getEpochSecond();
+        && a.getDuration().equals(b.getDuration())
+        && a.priority.equals(b.priority);
   }
 }
