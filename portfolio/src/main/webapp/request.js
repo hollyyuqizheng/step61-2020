23156113445b5e568ff337335e72ed8299076e01
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-07-07 14:10:51 -0400

    update param comments

--- portfolio/src/main/webapp/request.js
+++ portfolio/src/main/webapp/request.js
@@ -7,16 +7,21 @@
 class ScheduleRequest {
   /**
    * All fields are required for this object.
-   * @param events: events happening the day of the scheduling,
-   * @param tasks: the tasks a user would like to schedule on that day
+   * @param events: events happening the day of the scheduling. This is an
+   *     array of JavaScript CalendarEvent objects.
+   * @param tasks: the tasks a user would like to schedule on that day. This is an
+   *     array of JavaScript Task objects.
    * @param startTime: a JavaScript Date object representing the start time.
    * @param endTime: a JavaScript Date object representing the end time.
+   * @param algorithmType: a string representation of the algorithm type that
+   *     the user can select fromt the UI.
    */
-  constructor(events, tasks, startTime, endTime) {
+  constructor(events, tasks, startTime, endTime, algorithmType) {
     this.events = events;
     this.tasks = tasks;
     this.startTime = startTime;
     this.endTime = endTime;
+    this.algorithmType = algorithmType;
   }
 }
 
