6d6f4709a03da7295d873d6fa5e39cd8ef6de593
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-07-08 17:35:35 -0400

    update request.js

--- portfolio/src/main/webapp/request.js
+++ portfolio/src/main/webapp/request.js
@@ -9,8 +9,8 @@ class ScheduleRequest {
    * All fields are required for this object.
    * @param events: events happening the day of the scheduling. This is an
    *     array of JavaScript CalendarEvent objects.
-   * @param tasks: the tasks a user would like to schedule on that day. This is an
-   *     array of JavaScript Task objects.
+   * @param tasks: the tasks a user would like to schedule on that day. This is
+   *     an array of JavaScript Task objects.
    * @param startTime: a JavaScript Date object representing the start time.
    * @param endTime: a JavaScript Date object representing the end time.
    * @param algorithmType: a string representation of the algorithm type that
@@ -25,4 +25,90 @@ class ScheduleRequest {
   }
 }
 
-// TODO(tomasalvarez): write function to fetch results and display results
+/**
+ * Gets all of the scheduling information from the UI and returns a
+ * ScheduleRequest with all of the data.
+ */
+function createRequestFromUiInformation() {
+  const startTime = document.getElementById('working-hour-start').value;
+  const endTime = document.getElementById('working-hour-end').value;
+  const events = collectAllEvents();
+  const tasks = collectAllTasks();
+  const algorithmType = document.getElementById('algorithm-type').value;
+  const scheduleRequest = new ScheduleRequest(
+      events, tasks, getTimeObject(startTime), getTimeObject(endTime),
+      algorithmType);
+  return scheduleRequest;
+}
+
+/**
+ * Gets called when the user hits 'Start Scheduling'.
+ */
+function startScheduling() {
+  const scheduleRequest = createRequestFromUiInformation();
+  // Create the request to send to the server using the data we collected from
+  // the web form.
+  collectAllScheduledTasks(scheduleRequest).then((scheduledTaskArray) => {
+    updateResultsOnPage(scheduledTaskArray);
+  });
+}
+
+/**
+ * Updates the UI to show the results of a query.
+ */
+function updateResultsOnPage(scheduledTaskArray) {
+  const resultElement = document.getElementById('scheduled-task-list');
+  resultElement.innerHTML = '';
+  scheduledTaskArray.forEach(addScheduledTaskToPage, resultElement);
+}
+
+/**
+ * Handles sending the ScheduleRequest object to the servlet and returns the
+ * scheduled tasks.
+ */
+function collectAllScheduledTasks() {
+  const scheduleRequest = createRequestFromUiInformation();
+  const json = JSON.stringify(scheduleRequest);
+  return fetch('/schedule', {method: 'POST', body: json}).then((response) => {
+    return response.json();
+  });
+}
+
+
+/**
+ * This takes in a single JSON object of a ScheduledTask (Java Class) and
+ * displays that information on the result element using a card format.
+ */
+function addScheduledTaskToPage(scheduledTaskWrapper, resultElement) {
+  const task = scheduledTaskWrapper.task;
+  const taskName = task.name;
+  const taskTimeSeconds = scheduledTaskWrapper.startTime.seconds;
+  const taskDate = new Date();
+  // This is x1000 because the functions takes milliseconds
+  taskDate.setTime(taskTimeSeconds * 1000);
+  const taskPriority = task.priority.priority;
+  resultElement.innerText += taskName;
+  resultElement.innerText += taskName;
+
+  const newResultCard = document.createElement('div');
+  newResultCard.classList.add('card');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  newResultCard.appendChild(cardBody);
+
+  const cardTitle = document.createElement('h4');
+  cardTitle.classList.add('card-title');
+  cardTitle.innerText = taskName;
+  cardBody.appendChild(cardTitle);
+
+  const timeText = document.createElement('p');
+  timeText.classList.add('card-text');
+  timeText.innerText = 'Scheduled for: ' + taskDate;
+  cardBody.appendChild(timeText);
+
+
+  const eventList = document.getElementById('scheduled-task-list');
+  eventList.innterHTML = '';
+  eventList.appendChild(newResultCard);
+}
