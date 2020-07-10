b2b210b5755aa678df464eaccb6b766d85baab5c
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-07-10 18:12:14 -0400

    update Sheets.Api.js

--- portfolio/src/main/webapp/sheetsApi.js
+++ portfolio/src/main/webapp/sheetsApi.js
@@ -1,18 +1,18 @@
 // Client ID and API key from the Developer Console
-const CLIENT_ID =
+const CLIENT_ID_SHEETS =
     '499747085593-hvi6n4kdrbbfvcuo1c9a9tu9oaf62cr2.apps.googleusercontent.com';
-const API_KEY = '';
+const API_KEY_SHEETS = '';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-const DISCOVERY_DOCS =
+const DISCOVERY_DOCS_SHEETS =
     ['https://sheets.googleapis.com/$discovery/rest?version=v4'];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-const SCOPE_READ_WRITE = 'https://www.googleapis.com/auth/spreadsheets';
+const SCOPE_READ_WRITE_SHEETS = 'https://www.googleapis.com/auth/spreadsheets';
 
 // Error codes that the process can throw
-const ERROR_CODE = {
+const ERROR_CODE_SHEETS = {
   POPUP_CLOSED: 'popup_closed_by_user',
   ACCESS_DENIED: 'access_denied'
 };
@@ -20,37 +20,39 @@ const ERROR_CODE = {
 /**
  * On load, called to load the auth2 library and API client library.
  */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+function handleClientLoadSheets() {
+  gapi.load('client:auth2', initClientSheets);
 }
 
 /**
  * Initializes the API client library and sets up sign-in state
  * listeners.
  */
-function initClient() {
+function initClientSheets() {
   gapi.client
       .init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPE_READ_WRITE
+        apiKey: API_KEY_SHEETS,
+        clientId: CLIENT_ID_SHEETS,
+        discoveryDocs: DISCOVERY_DOCS_SHEETS,
+        scope: SCOPE_READ_WRITE_SHEETS
       })
       .then(
           function() {
-            var exportButton = document.getElementById('sheets-export-button');
-            var signOutButton =
+            var exportButtonSheets =
+                document.getElementById('sheets-export-button');
+            var signOutButtonSheets =
                 document.getElementById('sheets-sign-out-button');
-            var signInButton = document.getElementById('sheets-sign-in-button');
+            var signInButtonSheets =
+                document.getElementById('sheets-sign-in-button');
 
             // Listen for sign-in state changes.
-            gapi.auth2.getAuthInstance().isSignedIn.listen(handleButtons);
+            gapi.auth2.getAuthInstance().isSignedIn.listen(handleUiSheets);
 
             // Handle the initial sign-in state.
-            handleButtons();
-            exportButton.onclick = handleExportSchedule;
-            signOutButton.onclick = handleSignOut;
-            signInButton.onclick = handleSignIn;
+            handleUiSheets();
+            exportButtonSheets.onclick = handleExportSchedule;
+            signOutButtonSheets.onclick = handleSignOut;
+            signInButtonSheets.onclick = handleSignIn;
           },
           function(error) {
             handleError(error);
@@ -78,7 +80,7 @@ function handleSignOut() {
 /**
  * Called when the sign-in state changes and updates the UI appropriately.
  */
-function handleButtons() {
+function handleUiSheets() {
   $('#sheets-export-button').addClass('d-none');
   if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
     $('#sheets-export-button').removeClass('d-none');
@@ -96,11 +98,11 @@ function handleButtons() {
  * message on the UI.
  */
 function handleAuthorizationError(error) {
-  var $sheetsMessage = document.getElementById('sheets-message');
+  var $sheetsMessage = $('#sheets-message');
   $sheetsMessage.removeClass('d-none');
-  if (error.error === ERROR_CODE.POPUP_CLOSED) {
+  if (error.error === ERROR_CODE_SHEETS.POPUP_CLOSED) {
     $sheetsMessage.text('You closed out of the popup, please log in again.');
-  } else if (error.error === ERROR_CODE.ACCESS_DENIED) {
+  } else if (error.error === ERROR_CODE_SHEETS.ACCESS_DENIED) {
     $sheetsMessage.text('You did not authorize Google Sheets.');
   } else {
     $sheetsMessage.text('An error occurred try again.');
@@ -113,10 +115,48 @@ function handleAuthorizationError(error) {
  * message on the UI.
  */
 function handleExportError(reason) {
-  var $sheetsMessage = document.getElementById('sheets-message');
-  $sheetsMessage.removeClass('d-none')
+  $('#sheets-message').removeClass('d-none')
       .text('Error: ' + reason.result.error.message)
       .show();
+}
+
+/**
+ * Helps the SheetsApi requests easily create the values it needs for a
+ * spreadsheet.
+ */
+function makeSheetsValuesFromScheduledTasks(scheduledTasks, values) {
+  // I think this code is shorter without a forEach() and I also do not
+  // know how to make this work with a forEach(). 
+  for (index = 0; index < scheduledTasks.length; index++) { 
+    values.push(singleScheduledTaskToSheetsArray(scheduledTasks[index])); 
+  } 
+}
+
+/**
+ * This takes in a single JSON object of a ScheduledTask (Java Class) and
+ * turns it into the correct format for the Sheets API
+ */
+function singleScheduledTaskToSheetsArray(scheduledTask) {
+  var scheduledTaskAsSheetsArray = [];
+
+  const task = scheduledTask.task;
+  const taskName = task.name;
+  // Changes seconds into minutes.
+  const taskDurationMinutes = task.duration.seconds/60;
+  const taskDescription = task.description.value;
+  const taskPriority = task.priority.priority;
+  const taskTimeSeconds = scheduledTask.startTime.seconds;
+  const taskDate = new Date();
+  // This is x1000 because the functions takes milliseconds
+  taskDate.setTime(taskTimeSeconds * 1000);
+
+  scheduledTaskAsSheetsArray.push(taskName);
+  scheduledTaskAsSheetsArray.push(taskDate.toString());
+  scheduledTaskAsSheetsArray.push(taskDurationMinutes);
+  scheduledTaskAsSheetsArray.push(taskDescription);
+  scheduledTaskAsSheetsArray.push(taskPriority);
+
+  return scheduledTaskAsSheetsArray;
 }
 
 /**
@@ -124,6 +164,15 @@ function handleExportError(reason) {
  * Also displays the link to the spreadsheet as text on the UI.
  */
 function handleExportSchedule() {
+  const scheduledTaskCount = scheduledTasks.length;
+  // If the person has yet to schedule something
+  if (scheduledTaskCount == 0) {
+    $('#sheets-message').removeClass('d-none')
+        .text('Cannot export empty schedule.')
+        .show();
+    return;
+  }
+
   var spreadsheetProperties = {
     title: 'Your Scheduled Tasks'
     // TODO(tomasalvarez): Add desired creation properties.
@@ -135,17 +184,29 @@ function handleExportSchedule() {
 
   var request = gapi.client.sheets.spreadsheets.create(
       {properties: spreadsheetProperties});
-  request.then(
-      function(response) {
-        // TODO(tomasalvarez): Change code below to process the `response` 
-        // object. Currently just displays the link back to the user.
-        $('#sheets-url-container').removeClass('d-none');
-        $('#sheets-url-container')
-            .text(
-                'Done. Here is the URL to your spreadsheet: ' +
-                response.result.spreadsheetUrl);
-      },
-      function(reason) {
-        handleExportError(reason);
-      });
+  request.then((response) => {
+    var values = [['Task', 'Scheduled Time', 'Duration (minutes)', 'Description', 'Priority']];
+    
+    makeSheetsValuesFromScheduledTasks(scheduledTasks, values);
+    console.log(values);
+    values.push([]);
+    values.push([scheduledTaskCount.toString()+" Tasks Total", "",  "=SUM(C2:C" + (scheduledTaskCount+1).toString() + ") & \" Total Minutes Scheduled\""])
+
+    var data = {
+      // This is +3 because we have the initial row with titles and the final
+      // rows with some totals
+      'range': 'Sheet1!A1:E' + (scheduledTaskCount+3).toString(),
+      'majorDimension': 'ROWS',
+      'values': values
+    }  // Additional ranges to update.
+
+    var body = {data: data, valueInputOption: 'USER_ENTERED'};
+    gapi.client.sheets.spreadsheets.values
+        .batchUpdate(
+            {spreadsheetId: response.result.spreadsheetId, resource: body})
+        .then((response) => {
+          var result = response.result;
+          console.log(`${result.totalUpdatedCells} cells updated.`);
+        });
+  });
 }
