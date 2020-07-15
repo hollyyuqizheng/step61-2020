ca993d90839116e252548a662c6b2d6c551bdaa7
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-07-15 18:02:28 -0400

    update ScheduledTask.java

--- portfolio/src/main/java/com/google/sps/data/ScheduledTask.java
+++ portfolio/src/main/java/com/google/sps/data/ScheduledTask.java
@@ -2,6 +2,10 @@ package com.google.sps.data;
 
 import java.time.Instant;
 
+/**
+ * This is a wrapper class for scheduled tasks. Includes a Task and the Instant
+ * when it is scheduled.
+ */
 public class ScheduledTask {
   private final Task task;
   private final Instant startTime;
@@ -23,5 +27,14 @@ public class ScheduledTask {
 
   public Instant getStartTime() {
     return startTime;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    return other instanceof ScheduledTask && equals(this, (ScheduledTask) other);
+  }
+
+  public static boolean equals(ScheduledTask a, ScheduledTask b) {
+    return a.task.equals(b.task) && a.startTime.equals(b.startTime);
   }
 }
