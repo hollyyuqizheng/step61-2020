9892fc064c6dbe76f0fe012a2d2976d4166a28a1
Raul Cruz <raulcollege2@yahoo.com>
2020-07-29 20:42:05 +0000

    Add test to verify correct sorting

--- portfolio/src/test/java/com/google/sps/TaskQueueTest.java
+++ portfolio/src/test/java/com/google/sps/TaskQueueTest.java
@@ -1,6 +1,7 @@
 package com.google.sps.data;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Assert;
@@ -11,15 +12,16 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class TaskQueueTest {
-  private static final SchedulingAlgorithmType ALGORITHM_TYPE =
+  private static final SchedulingAlgorithmType SHORTEST_TASK_FIRST =
       SchedulingAlgorithmType.SHORTEST_TASK_FIRST;
 
+  private static Duration DURATION_15_MINUTES = Duration.ofSeconds(15 * 60);
+  private static Duration DURATION_20_MINUTES = Duration.ofSeconds(20 * 60);
   private static Duration DURATION_30_MINUTES = Duration.ofSeconds(30 * 60);
+  private static Duration DURATION_45_MINUTES = Duration.ofSeconds(45 * 60);
+  private static Duration DURATION_60_MINUTES = Duration.ofSeconds(60 * 60);
 
   private static final int DEFAULT_QUEUE_SIZE = 15;
-
-  private static final Comparator<Task> sortByTaskDurationThenName =
-      Comparator.comparing(Task::getDuration).thenComparing(Task::getName);
   
   private final TaskPriority LOW_PRIORITY = new TaskPriority(1);
 
@@ -31,7 +33,7 @@ public final class TaskQueueTest {
    */
   @Test(expected = IllegalArgumentException.class)
   public void passNullForList() {
-    TaskQueue taskQueue = new TaskQueue(null, ALGORITHM_TYPE);
+    TaskQueue taskQueue = new TaskQueue(null, SHORTEST_TASK_FIRST);
   }
 
   /**
@@ -46,5 +48,37 @@ public final class TaskQueueTest {
 
     List<Task> taskList = Arrays.asList(task1, task2, task3);
     TaskQueue taskQueue = new TaskQueue(taskList, null);
+  }
+
+  /**
+   * Ensure that the PriorityQueue is correctly sorting the tasks.
+   */
+  @Test
+  public void checkCorrectSortingShortestTaskFirst() {
+    Task task1 = new Task("Task 1", "First task", DURATION_15_MINUTES, LOW_PRIORITY);
+    Task task2 = new Task("Task 2", "Second task", DURATION_20_MINUTES, LOW_PRIORITY);
+    Task task3 = new Task("Task 3", "Third task", DURATION_30_MINUTES, LOW_PRIORITY);
+    Task task4 = new Task("Task 4", "Fourth task", DURATION_45_MINUTES, LOW_PRIORITY);
+    Task task5 = new Task("Task 5", "Fifth task", DURATION_60_MINUTES, LOW_PRIORITY);
+
+    List<Task> taskList = Arrays.asList(task3, task1, task2, task5, task4);
+
+    List<Task> expected = Arrays.asList(task1, task2, task3, task4, task5);
+    List<Task> actual = new ArrayList<Task>();
+
+    TaskQueue taskQueue = new TaskQueue(taskList, SHORTEST_TASK_FIRST);
+    
+    actual.add(taskQueue.peek());
+    taskQueue.remove();
+    actual.add(taskQueue.peek());
+    taskQueue.remove();
+    actual.add(taskQueue.peek());
+    taskQueue.remove();
+    actual.add(taskQueue.peek());
+    taskQueue.remove();
+    actual.add(taskQueue.peek());
+    taskQueue.remove();
+
+    Assert.assertEquals(expected, actual);
   }
 }
