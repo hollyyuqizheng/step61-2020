f77cfa61a6aa376e4f3acb4579b8ba0642f17358
Raul Cruz <raulcollege2@yahoo.com>
2020-08-03 20:59:57 +0000

    Change the way the algorithm accesses availableTimeRanges to a cleaner format

--- portfolio/src/main/java/com/google/sps/data/HighestPriorityFirstScheduler.java
+++ portfolio/src/main/java/com/google/sps/data/HighestPriorityFirstScheduler.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,25 +29,16 @@ public class HighestPriorityFirstScheduler implements TaskScheduler {
 
     List<TimeRange> availableTimes = calendarEventsGroup.getFreeTimeRanges();
     TimeRangeGroup availableTimesGroup = new ArrayListTimeRangeGroup(availableTimes);
+    List<TimeRange> currentAvailableTimes = availableTimeRangesList(availableTimesGroup);
 
     List<ScheduledTask> scheduledTasks = new ArrayList<ScheduledTask>();
 
-    Iterator<TimeRange> availableTimesIterator = availableTimesGroup.iterator();
+    int availableTimesIndex = 0;
 
     Instant currentScheduleTime = workHoursStartTime;
 
-    Boolean getNextTimeRange = true;
-
-    // Set the first availableTimeRange using the first list value
-    // in order to avoid getting the value from the availableTimesIterator
-    // which would prevent the while loop from running if there were to
-    // only be a single availableTimeRange.
-    TimeRange availableTimeRange = availableTimes.get(0);
-
-    while (availableTimesIterator.hasNext() && !taskQueue.isEmpty()) {
-      if (getNextTimeRange) {
-        availableTimeRange = availableTimesIterator.next();
-      }
+    while (availableTimesIndex < currentAvailableTimes.size() && !taskQueue.isEmpty()) {
+      TimeRange availableTimeRange = currentAvailableTimes.get(availableTimesIndex);
       Task task = taskQueue.peek();
 
       if (availableTimeRange.start().isAfter(currentScheduleTime)) {
@@ -70,32 +60,31 @@ public class HighestPriorityFirstScheduler implements TaskScheduler {
                 scheduledTask
                     .getStartTime()
                     .plusSeconds(scheduledTask.getTask().getDuration().getSeconds())));
+        // After deleting a TimeRange, we must renew the list.
+        currentAvailableTimes = availableTimeRangesList(availableTimesGroup);
 
         // Push the currentScheduleTime back by the amount that was scheduled.
         currentScheduleTime = currentScheduleTime.plusSeconds(task.getDuration().getSeconds());
         taskQueue.remove();
 
         // If the next task's priority is different from the task that was just scheduled,
-        // then reset the availableTimesIterator, currentScheduleTime, and make the loop
+        // then reset the availableTimesIndex, currentScheduleTime, and make the loop
         // grab a TimeRange. We go back to the first availableTimeRange in order to schedule
         // more tasks towards the beginning of the availableTimes.
         if (!taskQueue.isEmpty()
             && taskQueue.peek().getPriority().getPriority() != task.getPriority().getPriority()) {
-          availableTimesIterator = availableTimesGroup.iterator();
+          availableTimesIndex = 0;
           currentScheduleTime = workHoursStartTime;
-          getNextTimeRange = true;
-        } else {
-          // If the task's priority is not different, then we can simply continue running the
-          // the loop without retrieving the next availableTimeRange since there might be
-          // some time left over towards the end of the current one.
-          getNextTimeRange = false;
         }
-      } else if (!availableTimesIterator.hasNext()) {
+        // If the task's priority is not different, then we can simply continue running the
+        // the loop without retrieving the next availableTimeRange since there might be
+        // some time left over towards the end of the current one.
+      } else if (availableTimesIndex == currentAvailableTimes.size() - 1) {
         // If we've reached the end of the availableTimeRange group and we can't schedule a task,
         // then we can remove all the remaining tasks of equal priority since they will all be
         // longer in duration therefore, they will not be able to be scheduled either.
         if (!taskQueue.isEmpty()) {
-          availableTimesIterator = availableTimesGroup.iterator();
+          availableTimesIndex = 0;
           while (!taskQueue.isEmpty()
               && taskQueue.peek().getPriority().getPriority() == task.getPriority().getPriority()) {
             taskQueue.remove();
@@ -104,12 +93,18 @@ public class HighestPriorityFirstScheduler implements TaskScheduler {
       } else {
         // Get the next TimeRange if the Task cannot be scheduled but we aren't at the final
         // TimeRange.
-        getNextTimeRange = true;
+        availableTimesIndex++;
       }
     }
 
     Collections.sort(scheduledTasks, sortByScheduledStartTimeAscending);
     return scheduledTasks;
+  }
+
+  private List<TimeRange> availableTimeRangesList(TimeRangeGroup updatedTimeRangeGroup) {
+    List<TimeRange> availableTimes = new ArrayList();
+    updatedTimeRangeGroup.iterator().forEachRemaining(availableTimes::add);
+    return availableTimes;
   }
 
   /** Returns the scheduler's type, which is Highest Priority First. */
